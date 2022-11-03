dbafa5b6579b52a8474b61c4b01a79930060f98e
liran <msdrlab@gmail.com>
2022-11-03 15:27:05 +0200

    try

--- Dash_Gui_client.py
+++ Dash_Gui_client.py
@@ -8,28 +8,22 @@ PORT = 65432  # The port used by the server
 BUFFER_SIZE=4096
 path=os.getcwd()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    phase_counter=0
+    s.connect((HOST, PORT))
+    value1_from_host=s.recv(1024)
+    file_name=bytes.decode(value1_from_host)
+    print(f"we recieved frm raspberry pi {file_name}")
+    s.sendall(b"host is waiting for file size")
+    value2_from_host=s.recv(1024)
+    filesize=bytes.decode(value2_from_host)
+    print(f"the file size is {filesize}")
+    new_file=open(file_name,"w")
+    s.sendall(b"host is waiting for file to be send ")
+    
     while True:
-        if (phase_counter == 0):
-            #open communication and receive the file name and type and open file 
-            # with the same name
-            s.connect((HOST, PORT))
-            value1_from_host=s.recv(1024)
-            print(f"we want to open {value1_from_host}")
-            file_name=bytes.decode(value1_from_host)
-            print(f"we want to open {file_name}")
-            print(f"you are on {path}")
-            new_file=open(file_name,"w")
-            s.sendall(b"host clear to go to phase 1")
-            phase_counter+=1
-        if (phase_counter == 1):
-            # s.connect((HOST, PORT))
-            value2_from_host=s.recv(BUFFER_SIZE)
+            value3_from_host=s.recv(filesize)
             new_file.write(bytes.decode(value2_from_host))
-            phase_counter+=1
-            s.sendall(b"host clear to go to final phase and finish call back")
-        if (phase_counter == 2):
-            break
+            s.sendall(b"raspberry pi function protocol is complete")
+   
 
   
 
